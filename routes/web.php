e1a38d892703ddf1d8f8680c8e819394434145b9
mimi84317 <mimi84317@gate.sinica.edu.tw>
2021-05-06 10:05:24 +0800

    更新composer

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    //return view('uploadFilelist');
-    //return view('testhtml');
 });
 
 Route::group([
